efc356eef0965ddadbd9073db3c317f872fce45f
yonatan shavat <yshavat89@gmail.com>
2018-10-23 06:07:23 +0300

    login page works still in test

--- smartsced1/authentication/views.py
+++ smartsced1/authentication/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.views.generic import TemplateView
 
 # Create your views here.
+class LoginView(TemplateView):
+    template_name = 'authentication/login.html'
+
+class SignInView(View):
+    pass
+
+class SignOutView(View):
+    pass
+
