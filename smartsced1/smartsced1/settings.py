299cbc4f767f842cf2c458dc69027001ebbc8019
yonatan shavat <yshavat89@gmail.com>
2018-10-23 19:00:57 +0300

    add DIRS path to template object in settings.py

--- smartsced1/smartsced1/settings.py
+++ smartsced1/smartsced1/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'smartsced1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'static')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
